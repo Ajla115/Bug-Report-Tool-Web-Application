bfb80a8d70018cc54a9a549ed5208b680806fabf
Ajla Korman <ajla.korman@stu.ibu.edu.ba>
2023-10-07 11:28:00 +0200

    Installed Mongoose, connected to database

--- README.md
+++ README.md
@@ -14,3 +14,8 @@ Notes:
 10. Sad u package dependencies prepraviti da umjesto node bude nodemon da se sad sve promjene automatski izvrsavaju da ne bismo morali svaki put ispocetka server pokretati, "dev": "nodemon index.js"
 11. Prije smo imali slucaj da smo u terminal mogli kucati node index.js ali to ne bismo mogli i sa nodemon index.js jer njega nemamo globalno install na laptopu vec samo na ovome projektu i pozivamo ga onda preko keyworda iz skripte iz package.json koja onda zna sta treba raditi
 12. U Postmanu se mogu sve rute testirati, i uvijek se koristi uz backend za testiranje
+13. Instaliranje body parsera npm install body-parser i naravno treba ga import u index.js
+14. Parser se koristi da pravljenje body propertija i ubacivanje vrijendosti negdje, kao kad posaljemo neke podatke koji trebaju u nodejs body posto nodejs by default nema taj body, ova opcija body parser ga pravi i onda se ti podaci imaju gdje ubaciti
+15. Mongoose smo instalirali npm install mongoose i importovali ga na pocetak node.js jer se sve dependencies importuju na vrh na pocetak i nakon toga smo odradili konekciju preko mongoose.connect('mongodb://127.0.0.1:27017/bug-report')
+  .then(() => console.log('Connected to the database!'));
+16. Ovo je Mongoose -> Mongoose is an Object Data Modeling (ODM) library for MongoDB and Node. js. It manages relationships between data, provides schema validation, and is used to translate between objects in code and the representation of those objects in MongoDB
--- server/index.js
+++ server/index.js
@@ -1,21 +1,39 @@
 //const express = require('express') //older version to import
 
-import express from 'express'        //imported express
+import express from 'express';       //imported express
                                      //IMPORTANT: for this to work add "type" : "module",  in package.json!
+
+import bodyParser from 'body-parser';   
+import mongoose from 'mongoose';                             
 
 const app = express()                //declared express app
 const PORT = 4000                    //declared main port
 
 //it is very beneficial to write const variables with a capital letter
 
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// parse application/json
+app.use(bodyParser.json());
+
+
+
 //this is how you write routes in node, eg. app.- post, put, delete, get
 //req - request, res - response, next - next
 //through request you send data for something, for login ect.
 //response - is the result
 app.get('/', (req, res) => {
+    console.log(req.body); 
+    //this line of code will be the one that will print out the sent data as its body because of an implementation of a body parser, 
+    //if there was no body parser it would not be possible to this since Nodejs does not have body by default
   res.send('Flower is colorful!')
   res.status(200).send("Success")
 });  //always write ; at the end of a function in node 
+
+//Models for queries, findByID, getOne ect, use it for making schemas etc
+mongoose.connect('mongodb://127.0.0.1:27017/bug-report')
+  .then(() => console.log('Connected to the database!'));
 
 //callback function to know something is working
 app.listen(PORT, () => {
